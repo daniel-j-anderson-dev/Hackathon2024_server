3a8d2fe8c50453212a7a9f5c45d4cf22cae77226
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-02-23 14:02:20 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+mod games;
+
+use axum::{routing::get, Router};
+use color_eyre::{eyre::eyre, Report};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Report> {
+    let ip = "localhost:8080";
+
+    let listener = TcpListener::bind(&ip).await?;
+
+    let router = Router::new().route("/", get(hello_world));
+
+    println!("Listening on http://{ip}");
+    axum::serve(listener, router).await?;
+
+    return Ok(());
+}
+
+async fn hello_world() -> &'static str {
+    return "Hello, 🌎!";
+}
